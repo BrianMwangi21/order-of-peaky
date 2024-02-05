14dc57ffba2bba8caa93e1fa58a817363d4791c4
kabiru <mwangikabiru21@gmail.com>
2024-02-05 14:40:44 +0300

    feat : Custom logger

--- utils/handlers.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "log"
-
-func HandleError(message string, err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
--- utils/localbook.go
+++ utils/localbook.go
@@ -2,9 +2,10 @@ package utils
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
 	"sync"
 
 	"github.com/adshao/go-binance/v2"
@@ -23,19 +24,31 @@ var (
 	doneC, stopC          chan struct{}
 	wsErr                 error
 	ORDER_BOOK_SIZE_LIMIT = 100
+	peaky                 *log.Logger
 )
+
+func init() {
+	peaky = log.New(os.Stdout, "peaky:", log.LstdFlags|log.Lshortfile)
+	peaky.Println("Logger initialized")
+}
 
 func getDepthSnapshot(symbol string) DepthSnapshot {
 	var snapshot DepthSnapshot
 	resp, err := http.Get("https://api.binance.com/api/v3/depth?symbol=" + symbol + "&limit=5000")
-	HandleError("Error fetching snapshot from API:", err)
+	if err != nil {
+		peaky.Fatalf("Error fetching snapshot from API: %v", err)
+	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
-	HandleError("Error reading response body:", err)
+	if err != nil {
+		peaky.Fatalf("Error reading response body: %v", err)
+	}
 
 	err = json.Unmarshal(body, &snapshot)
-	HandleError("Error unmarshalling depth snapshot:", err)
+	if err != nil {
+		peaky.Fatalf("Error unmarshalling depth snapshot: %v", err)
+	}
 
 	return snapshot
 }
@@ -45,19 +58,22 @@ func wsDepthHander(event *binance.WsDepthEvent) {
 	defer orderBookLock.Unlock()
 
 	if event.LastUpdateID <= lastUpdateId {
+		peaky.Println("Event received and ignored:", event.LastUpdateID, len(event.Bids), len(event.Asks))
 		return
 	}
 
 	if len(orderBook) < ORDER_BOOK_SIZE_LIMIT {
 		orderBook = append(orderBook, event)
-		fmt.Println("Event received and processed:", event.LastUpdateID, len(event.Bids), len(event.Asks))
+		peaky.Println("Event received and processed:", event.LastUpdateID, len(event.Bids), len(event.Asks))
 	} else {
 		stopC <- struct{}{}
 	}
 }
 
 func wsErrorHandler(err error) {
-	HandleError("WsDepthServe error:", err)
+	if err != nil {
+		peaky.Fatalf("WsDepthServe error: %v", err)
+	}
 }
 
 func ManageLocalOrderBook(symbol string) {
@@ -66,12 +82,14 @@ func ManageLocalOrderBook(symbol string) {
 	lastUpdateId = snapshot.LastUpdateId
 	orderBookLock.Unlock()
 
-	fmt.Println("Depth Snapshot lastUpdateId:", lastUpdateId)
+	peaky.Println("Depth Snapshot lastUpdateId:", lastUpdateId)
 
 	doneC, stopC, wsErr = binance.WsDepthServe(symbol, wsDepthHander, wsErrorHandler)
-	HandleError("Error launching WsDepthServe websocket:", wsErr)
+	if wsErr != nil {
+		peaky.Fatalf("Error launching WsDepthServe websocket: %v", wsErr)
+	}
 
 	<-doneC
 
-	fmt.Println("Orderbook len", len(orderBook))
+	peaky.Println("Orderbook length", len(orderBook))
 }
